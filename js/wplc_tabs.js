6e899995f1cc185ba2ad7fdd662cce4334011b49
WP-LiveChat <WP-LiveChat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-05 06:22:39 +0000

    * 5.0.1 - 2015-08-05 - Medium Priority  * Refactored the code so that the live chat box will show up even if there is a JS error from another plugin or theme  * Live chat box styling fixes: top image padding; centered the "conneting, please be patient" message and added padding  * The live chat long poll connection will not automatically reinitialize if the connection times out or returns a 5xx error
    git-svn-id: https://plugins.svn.wordpress.org/wp-live-chat-support/tags/5.0.0@1213157 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wplc_tabs.js
@@ -0,0 +1,11 @@
+jQuery("document").ready(function() {
+   jQuery("#wplc_tabs").tabs();
+   
+   
+   
+    jQuery(function () {
+        jQuery(".wplc_settings_tooltip").tooltip({
+            position: {my: "left+15 center", at: "right center"}
+        });
+    });
+});
